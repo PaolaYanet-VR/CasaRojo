16a06780530eae9db39aa1cdfccfb4b11639f3de
Yanet Rojo <yanetrojo01@gmail.com>
2021-11-19 12:22:38 -0600

    Vendedor (Clases y JDBC) y Conexión

--- CasaRojoAdmin/src/main/java/datos/VendedorDaoJDBC.java
+++ CasaRojoAdmin/src/main/java/datos/VendedorDaoJDBC.java
@@ -1,14 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datos;
 
-/**
- *
- * @author 15-dk0007la
- */
+import dominio.Vendedor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendedorDaoJDBC {
+    private static final String SQL_SELECT = "SELECT idvendedor, usuario, password "
+            + " FROM vendedor";
+
+    private static final String SQL_SELECT_BY_USER = "SELECT idvendedor, usuario, password "
+            + " FROM vendedor WHERE usuario = ? AND password = ?";
+    
+    public List<Vendedor> listar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Vendedor vendedor = null;
+        List<Vendedor> vendedores = new ArrayList<>();
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idVendedor = rs.getInt("idvendedor");
+                String usuario = rs.getString("usuario");
+                String password = rs.getString("password");
+
+                vendedor = new Vendedor(idVendedor, usuario, password);
+                vendedores.add(vendedor);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return vendedores;
+    }
+    
+    public Vendedor encontrar(Vendedor vendedor) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT_BY_USER);
+            stmt.setString(1, vendedor.getUsuario());
+            stmt.setString(2, vendedor.getPassword());
+            
+            rs = stmt.executeQuery();
+            
+            rs.absolute(1);//nos posicionamos en el primer registro devuelto
+
+            int idVendedor = rs.getInt("idvendedor");
+            String usuario = rs.getString("usuario");
+            String password = rs.getString("password");
+            
+            vendedor.setIdVendedor(idVendedor);
+            vendedor.setUsuario(usuario);
+            vendedor.setPassword(password);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return vendedor;
+    }
     
 }
--- CasaRojoAdmin/src/main/java/dominio/Vendedor.java
+++ CasaRojoAdmin/src/main/java/dominio/Vendedor.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dominio;
 
-/**
- *
- * @author 15-dk0007la
- */
 public class Vendedor {
-    
+    private int idVendedor;
+    private String usuario;
+    private String password;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(String usuario, String password) {
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public Vendedor(int idVendedor, String usuario, String password) {
+        this.idVendedor = idVendedor;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" + "idVendedor=" + idVendedor + ", usuario=" + usuario + ", password=" + password + '}';
+    }
+
 }
